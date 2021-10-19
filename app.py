01269b850dd89131701ed9d9e1f9b35fc4e0263e
mgreenberry <michaelgreenberry@gmail.com>
2021-10-19 19:50:41 +0000

    added Procfile and requirements.txt

---
+++ app.py
@@ -0,0 +1,33 @@
+"""
+The following Python code is taken from the 'Task' mini-project.
+This imports flask, bson and werkzeug for password control.
+A env.py file will also be created which will hold the MongoDB details
+"""
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+            """
+            This must be changed to False before submission!
+            """
+            
