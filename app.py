b93d1a6c88d2191a90a5005f845ff31637be56d8
mgreenberry <michaelgreenberry@gmail.com>
2021-10-22 16:06:57 +0000

    added a food_list.html page to list the foods that users add. Edited app.py and base.html

--- app.py
+++ app.py
@@ -17,12 +17,153 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
 @app.route("/")
-@app.route("/get_food")  # is this the right route?
+@app.route("/food")
+def foods():
+    foods = mongo.db.food.find()
+    return render_template("home.html", foods=foods)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("home", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "foods", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("food.html", username=username)
+
+    return render_template("food.html", username=username)
+
+
+@app.route("/logout")
+def logout():
+    # Logs user out
+    flash("You have logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+@app.route("/add_food", methods=["GET", "POST"])
+def add_food():
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        food = {
+            "location": request.form.get("location"),
+            "food_name": request.form.get("food_name"),
+            "barcode": request.form.get("barcode"),
+            "use_by_date": request.form.get("use_by_date"),
+            "purchase_date": request.form.get("purchase_date"),
+            "is_urgent": is_urgent,
+            "created_by": session["user"]
+        }
+        mongo.db.food.insert_one(food)
+        flash("Food added succesfully")
+        return redirect(url_for("add_food"))
+        
+    foods = mongo.db.food.find().sort("food_name", 1)
+    return render_template("add_food.html", foods=foods)
+
+
+@app.route("/edit_food/<food_name>", methods=["GET", "POST"])
+def edit_food(food_name):
+
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "food_name": request.form.get("food_name"),
+            "food_group": request.form.get("food_group"),
+            "use_by_date": request.form.get("use_by_date"),
+            "barcode": request.form.get("barcode"),
+            "is_urgent": is_urgent,
+            "created_by": session["user"]
+        }
+        mongo.db.food.update({"_id": ObjectId(food_name)}, submit)
+        flash("Food updated succesfully")
+
+    food = mongo.db.food.find_one({"_id": ObjectId(food_name)})
+    foods = mongo.db.food.find().sort("food_name", 1)
+    return render_template("edit_food.html", food=food, foods=foods)
+
+
+@app.route("/delete_food/<food_name>")
+def delete_food(food_name):
+    mongo.db.catergories.remove({"_id": ObjectId(food_name)})
+    flash("Food Deleted")
+    return redirect(url_for("food"))
+
+
+@app.route("/home")
+def modifies():
+    modifies = list(mongo.db.catergories.find().sort('use_by_date', 1))
+    return render_template("home.html", modifies=modifies)
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+
+
+"""
+@app.route("/")
+@app.route("/food")  # is this the right route?
 def get_food():
     foods = mongo.db.food.find()
-    return render_template("food.html", foods=foods)
+    return render_template("home.html", foods=foods)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -99,28 +240,14 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_food", methods=["GET", "POST"])
-def add_food():
-    if request.method == "POST":
-        is_urgent = "on" if request.form.get("is_urgent") else "off"
-        food = {
-            "location": request.form.get("location"),
-            "food_name": request.form.get("food_name"),
-            "barcode": request.form.get("barcode"),
-            "use_by_date": request.form.get("use_by_date"),
-            "purchase_date": request.form.get("purchase_date"),
-            "is_urgent": is_urgent,
-            "created_by": session["user"]
-        }
-        mongo.db.food.insert_one(food)
-        flash("Food added succesfully")
-        return redirect(url_for("add_food"))
-        
-    foods = mongo.db.food.find().sort("food_group", 1)
-    return render_template("add_food.html", foods=foods)
+@app.route("/home")
+def home():
+    home = list(mongo.db.food.find().sort('use_by_date', 1))
+    return render_template("home.html", home=home)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+"""
