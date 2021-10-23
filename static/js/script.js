46d14e2b8a8b0917b84fb09b33557e3817833655
mgreenberry <michaelgreenberry@gmail.com>
2021-10-23 14:58:41 +0000

    Deleted code, modal, register py, food.html, an old food_list.html and login.html as no longer required. Others amended for these changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
+    $(".sidenav").sidenav({edge: "right"});
+    $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
+    $('select').formSelect();
     $('.modal').modal();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: "Select"
+      }
+    });
   });
